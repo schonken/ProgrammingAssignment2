31e70deadd091278cd76264d0151c7eee77ca40c
Francois Schonken <francois.schonken@gmail.com>
2014-06-06 11:40:16 +1000

    Added an usage section to the bottom of the script

--- example.R
+++ example.R
@@ -1,5 +1,5 @@
-## The following script implments the example functions
-## illustrating the use og the "<<-" operator
+## This script implements the example functions illustrating the 
+## use of the "<<-" operator
 
 makeVector <- function(x = numeric()) {
     m <- NULL
@@ -21,8 +21,22 @@ cachemean <- function(x, ...) {
         message("getting cached data")
         return(m)
     }
+    
+    message("calculating and creating cached data")
     data <- x$get()
     m <- mean(data, ...)
     x$setmean(m)
     m
 }
+
+## Just a bit of script to test if all is well in the land of mean caching
+v <- makeVector(44:63)
+cat("\n")
+print(cachemean(v))
+cat("\n")
+print(cachemean(v))
+cat("\n")
+print(mtrx$get())
+cat("\n")
+print(mtrx$getmean())
+cat("\n")
